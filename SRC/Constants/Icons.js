6a9003158cc4fa28c3bc179f23854e5761ffb2bd
dolappy <blademola121@gmail.com>
2023-05-19 15:54:34 +0100

    first commmit

---
+++ SRC/Constants/Icons.js
@@ -0,0 +1,11 @@
+export const back = (require = "../../assets/icons/back.png");
+export const bell = (require = "../../assets/icons/bell.png");
+export const heart = (require = "../../assets/icons/heart.png");
+export const like = (require = "../../assets/icons/like.png");
+export const mail = (require = "../../assets/icons/mail.png");
+export const mic = (require = "../../assets/icons/mic.png");
+export const search = (require = "../../assets/icons/search.png");
+export const star = (require = "../../assets/icons/star.png");
+export const user = (require = "../../assets/icons/user.png");
+
+export default { back, bell, heart, like, mail, mic, search, start, user };
